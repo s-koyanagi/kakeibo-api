61fd4cf2f7597d2a4df79018fedded7647989290
s-koyanagi <kysh19931216@gmail.com>
2021-06-07 22:48:20 +0900

    add component scan

--- src/main/java/com/kysh/kakeibo/KakeiboApplication.java
+++ src/main/java/com/kysh/kakeibo/KakeiboApplication.java
@@ -2,8 +2,10 @@ package com.kysh.kakeibo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.kysh.kakeibo","com.kysh.commons"})
 public class KakeiboApplication {
     public static void main(String[] args) {
         SpringApplication.run(KakeiboApplication.class, args);
