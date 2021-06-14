28693d42b514ea23ffe3a530495e554ec4c3f74f
s-koyanagi <kysh19931216@gmail.com>
2021-06-14 23:04:30 +0900

    apply format

--- src/main/java/com/kysh/kakeibo/auth/AuthSuccessHandlerImpl.java
+++ src/main/java/com/kysh/kakeibo/auth/AuthSuccessHandlerImpl.java
@@ -4,10 +4,8 @@ import com.kysh.commons.auth.AuthSuccessHandler;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
 public class AuthSuccessHandlerImpl implements AuthSuccessHandler {
